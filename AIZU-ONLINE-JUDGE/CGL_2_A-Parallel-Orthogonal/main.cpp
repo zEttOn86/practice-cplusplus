4944cf75140e34d1c7d1cd6d6d43d5b876793ad5
zEttOn86 <38281934+zEttOn86@users.noreply.github.com>
2018-09-02 23:49:10 +0900

    20180902

--- AIZU-ONLINE-JUDGE/CGL_2_A-Parallel-Orthogonal/main.cpp
+++ AIZU-ONLINE-JUDGE/CGL_2_A-Parallel-Orthogonal/main.cpp
@@ -1,5 +1,58 @@
 #include<iostream>
-#include"vector.hpp"
+#include<cmath>
+
+#define EPS (1e-10)
+#define equals(a, b) (fabs((a)-(b))<EPS)
+
+class Point{
+    public:
+    double x, y;
+
+    Point(double x=0, double y=0):x(x), y(y){}
+
+    Point operator + (Point p) {return Point(x+p.x, y+p.y); }
+    Point operator - (Point p) {return Point(x-p.x, y-p.y); }
+    Point operator * (double a) {return Point(a*x, a*y); }
+    Point operator / (double a) {return Point(a*x, a*y); }
+
+    double abs() {return sqrt(norm());}
+    double norm() {return x*x+y*y; }
+
+    bool operator < (const Point &p) const{
+        return x != p.x ? x<p.x: y<p.y;
+    }
+
+    bool operator == (const Point &p) const{
+        return fabs(x-p.x) < EPS && fabs(y-p.y) < EPS;
+    }
+};
+
+typedef Point Vector;
+
+double dot(Vector a, Vector b){
+    return a.x * b.x+a.y*b.y;
+}
+
+double cross(Vector a, Vector b){
+    return a.x*b.y -a.y*b.x;
+}
+
+bool isOrthogonal(Vector a, Vector b){
+    return equals(dot(a,b), 0.);
+}
+
+bool isOrthogonal(Point a1, Point a2, Point b1, Point b2){
+    return isOrthogonal(a1-a2, b1-b2);
+}
+
+bool isParallel(Vector a, Vector b){
+    return equals(cross(a,b), 0.0);
+}
+
+bool isParallel(Point a1, Point a2, Point b1, Point b2){
+    return isParallel(a1-a2, b1-b2);
+}
+
 using namespace std;
 
 int main(int argc, char const *argv[])
@@ -17,5 +70,6 @@ int main(int argc, char const *argv[])
         }
         else printf("0\n");
     }
+
     return 0;
 }
